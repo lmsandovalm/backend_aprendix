ff76dfd10a65d7e47a1d8c676bb2919a7dfa27f8
StuxNet <juanpagosa@misena.edu.co>
2024-02-20 20:32:01 -0500

    Vistas

--- backend_aprendix/webservices/forms.py
+++ backend_aprendix/webservices/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from home.models import Docente
 
-
-
 # Definir un formulario para el modelo de datos de Docente
 class DocenteForm(forms.ModelForm):
     class Meta:
         model = Docente  # Especificar el modelo al que está asociado el formulario
         fields = ['nombre', 'correo', 'telefono', 'especialidad']  # Definir los campos del formulario
+
+        # Definir clases CSS para los campos del formulario
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo': forms.EmailInput(attrs={'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
+            'especialidad': forms.TextInput(attrs={'class': 'form-control'}),
+        }
